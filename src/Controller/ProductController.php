9249237fb93fd183469bc0513050c8045c136529
Matthieu Mota <matthieumota@gmail.com>
2020-01-30 15:45:52 +0100

    Edit and delete product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -67,5 +68,38 @@ class ProductController extends AbstractController
         return $this->render('product/list.html.twig', [
             'products' => $products,
         ]);
+    }
+
+    /**
+     * @Route("/product/edit/{id}", name="product_edit")
+     */
+    public function edit(Request $request, Product $product)
+    {
+        // On crée le formulaire avec le produit à modifier
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Met à jour l'objet dans la BDD
+            $this->getDoctrine()->getManager()->flush();
+
+            // Redirige vers la liste des produits après l'UPDATE
+            return $this->redirectToRoute('product_list');
+        }
+
+        return $this->render('product/edit.html.twig', [
+           'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/product/delete/{id}", name="product_delete")
+     */
+    public function delete(Product $product, EntityManagerInterface $entityManager)
+    {
+        $entityManager->remove($product);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('product_list');
     }
 }
