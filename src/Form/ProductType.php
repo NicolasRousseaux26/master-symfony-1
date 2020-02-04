c571c24c5c00b17fa37a93e942d93376d5ec279a
Matthieu Mota <matthieumota@gmail.com>
2020-02-04 10:00:33 +0100

    Category and user on product form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,6 +22,8 @@ class ProductType extends AbstractType
             ->add('price', MoneyType::class, [
                 'divisor' => 100
             ])
+            ->add('category')
+            ->add('user')
         ;
     }
 
